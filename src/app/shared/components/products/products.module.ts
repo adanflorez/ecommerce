baac0fae6813038585dde0f7e4c3b50e3baeaadf
Adan Florez <adan.florezb@gmail.com>
2022-01-10 20:29:03 -0500

    feat: lazy load image

--- src/app/shared/components/products/products.module.ts
+++ src/app/shared/components/products/products.module.ts
@@ -3,20 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { RouterModule } from '@angular/router';
-
-
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 @NgModule({
-  declarations: [
-    ProductsComponent,
-    ProductItemComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [
-    ProductsComponent
-  ]
+  declarations: [ProductsComponent, ProductItemComponent],
+  imports: [CommonModule, RouterModule, LazyLoadImageModule],
+  exports: [ProductsComponent],
 })
-export class ProductsModule { }
+export class ProductsModule {}
