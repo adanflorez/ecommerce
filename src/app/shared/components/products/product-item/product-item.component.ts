c977120a33d9c0649a52f6d5b3c7b5f16477f06b
Adan Florez <adan.florezb@gmail.com>
2022-01-11 17:20:14 -0500

    refactor: see product instead add to cart

--- src/app/shared/components/products/product-item/product-item.component.ts
+++ src/app/shared/components/products/product-item/product-item.component.ts
@@ -1,9 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
 import Product from 'src/app/core/model/product.model';
-import CartItem from 'src/app/core/model/cart-item.model';
-import { AppState } from 'src/app/store/app.state';
-import { addItemToCart } from 'src/app/store/products/actions/product.actions';
 
 @Component({
   selector: 'app-product-item',
@@ -13,12 +9,7 @@ import { addItemToCart } from 'src/app/store/products/actions/product.actions';
 export class ProductItemComponent implements OnInit {
   @Input() product: Product | undefined = undefined;
 
-  constructor(private store: Store<AppState>) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  public addItemToCart() {
-    const cartItem: CartItem = { product: this.product!, quantity: 1 };
-    this.store.dispatch(addItemToCart({ item: cartItem }));
-  }
 }
