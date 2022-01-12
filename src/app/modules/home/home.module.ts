6669be2e411ef3f6a4d33d1b5e5db59afd0ee4d5
Adan Florez <adan.florezb@gmail.com>
2022-01-11 19:56:25 -0500

    refactor: code was reorganized

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Routing module
 import { HomeRoutingModule } from './home-routing.module';
-import { ProductsPageComponent } from './pages/products/products.component';
+// Custom modules
 import { ProductsModule } from 'src/app/shared/components/products/products.module';
+// Pages
+import { ProductsPageComponent } from './pages/products/products.component';
 import { ProductsDetailPageComponent } from './pages/products-detail-page/products-detail-page.component';
-import { QuantityHandlerModule } from 'src/app/shared/components/quantity-handler/quantity-handler.module';
-
 
 @NgModule({
-  declarations: [
-    ProductsPageComponent,
-    ProductsDetailPageComponent,
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    ProductsModule,
-    QuantityHandlerModule
-  ]
+  declarations: [ProductsPageComponent, ProductsDetailPageComponent],
+  imports: [CommonModule, HomeRoutingModule, ProductsModule],
 })
-export class HomeModule { }
+export class HomeModule {}
